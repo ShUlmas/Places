21edc08991914e410830f92d84bf1e03ec04df00
ShUlmas <shamuratovolmasbek7@gmail.com>
2023-09-10 00:38:26 +0500

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by O'lmasbek on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
